51eed710ab2f8d5c82486fd8f08f54c4e59dbbf4
Sourav Kayal <sourav.kayal@yahoo.com>
2022-01-29 21:07:17 +0530

    first commit

---
+++ Controllers/PingController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XssValidatorUsage.Controllers
+{
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        
+        public UserController()
+        {
+
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser(User user)
+        {
+            return Ok();
+        }
+
+    }
+}
